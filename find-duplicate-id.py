6aba829d14b33150a5890f16c323c99cb3b13fd5
Hanif Rahmat <45779009+hanifrahmath@users.noreply.github.com>
2026-02-05 16:13:53 +0700

    find-duplicate-id.py

--- find-duplicate-id.py
+++ find-duplicate-id.py
@@ -1,5 +1,5 @@
-# kadang di references.bib ada duplicate IDs, dan ini jadi masalah pada sitasi
-# kode ini digunakan utuk menemukan duplicate IDs
+# kadang di references.bib ada duplicate IDs, dan ini jadi masalah pada sitasi di LaTeX
+# skrip Python ini digunakan utuk menemukan duplicate IDs
 
 def find_duplicate_bib_ids(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
